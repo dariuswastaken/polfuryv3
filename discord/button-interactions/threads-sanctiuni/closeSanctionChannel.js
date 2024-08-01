86a85fbd0783397d7585c39d9156a002e37cc7e7
halva1337 <valentindarius1702@gmail.com>
2024-08-01 12:13:11 +0200

    added interaction to close sanction channels

---
+++ discord/button-interactions/threads-sanctiuni/closeSanctionChannel.js
@@ -0,0 +1,36 @@
+module.exports = {
+  name: 'close-sanction-channel',
+  enabled: true,
+  async execute(pulsar, interaction, mongo, utils) {
+    await interaction.deferReply({ ephemeral: true });
+
+    const userID = interaction.customId.split('/')[1];
+    const sanctionID = interaction.customId.split('/')[2];
+
+    const transcript = await utils.quickFunctions.createChannelTranscript({
+      interaction: interaction,
+      channelID: interaction.channel.id,
+      type: 'Sanctiune'
+    });
+
+    const user = await interaction.guild.members.fetch(userID);
+
+    try {
+      await user.send({
+        content: `Sanctiunea ta a fost marcata ca si **REZOLVATA**.\nMai jos ai transcriptul sanctiunii tale.\n\n**ID Sanctiune:** ${sanctionID}`,
+        files: [
+          {
+            attachment: transcript,
+            name: `transcript-sanctiune-${userID}-${sanctionID}.html`
+          }
+        ]
+      });
+    } catch (e) {
+      console.log(
+        `[DISCORD ERROR] Could not send transcript to ${userID}\n${e.stack}`
+      );
+    }
+
+    await interaction.channel.delete();
+  }
+};
