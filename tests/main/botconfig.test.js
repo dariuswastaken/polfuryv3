ddfdba745d5ea4251525d720febceb07a6517c14
halva1337 <valentindarius1702@gmail.com>
2024-10-09 13:50:53 +0200

    changed botconfig test

--- tests/main/botconfig.test.js
+++ tests/main/botconfig.test.js
@@ -2,7 +2,12 @@ const botconfig = require('../../src/botconfig/botconfig.js');
 
 describe('botconfig', () => {
   it('should export an object', () => {
-    expect(botconfig).toBe(expect.any(Object));
+    expect(botconfig).toBeInstanceOf(Object);
     expect(botconfig).not.toBe(null);
   });
+  
+  it('should have the following properties', () => {
+    expect(botconfig).toHaveProperty('activityListMenusButtons');
+    expect(botconfig).toHaveProperty('demiterePendingMenus');
+  })
 });
