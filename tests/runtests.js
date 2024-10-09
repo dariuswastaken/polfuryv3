bf4ff9812b16fe75b8d4de57ea190e368d8ec1ba
halva1337 <valentindarius1702@gmail.com>
2024-10-09 13:31:30 +0200

    added separate code testing folder

---
+++ tests/runtests.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+const testDir = path.join(__dirname, 'tests');
+
+function runTests() {
+  fs.readdir(testDir, (err, files) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    files.forEach((file) => {
+      if (file.endsWith('.js')) {
+        require(path.join(testDir, file));
+        console.log(`[TEST] ${file} passed!`);
+      }
+    });
+  });
+}
