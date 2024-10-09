e44021dd091c206f24411809d0f4367b678f6bbb
halva1337 <valentindarius1702@gmail.com>
2024-10-09 13:25:18 +0200

    circular dependency fix (?)

--- index.js
+++ index.js
@@ -3,7 +3,4 @@ const { init } = require('./src/core/init.js');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const botconfig = require('./src/botconfig/botconfig.js');
-console.log(botconfig);
-
 init();
