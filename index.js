3dcc9a7a046c3f621a206c5e22511d4311aa505d
halva1337 <valentindarius1702@gmail.com>
2024-10-09 13:28:18 +0200

    removed testing code from index.js

--- index.js
+++ index.js
@@ -3,22 +3,4 @@ const { init } = require('./src/core/init.js');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const placeHolderModifier = require('./src/botconfig/placeholderModifier.js');
-const buttons = [
-  {
-    id: `menu-demitere/confirm/%targetid%/%uid%`,
-    style: 'Danger',
-    label: '✅ Confirmare'
-  },
-  {
-    id: `menu-demitere/cancel/%targetid%/%uid%`,
-    style: 'Secondary',
-    label: '❌ Anulare'
-  }
-]
-
-const newbuttons = placeHolderModifier.replaceButtonPlaceholders(buttons, { targetid: '123', uid: '456' });
-
-console.log(newbuttons);
-
 init();
