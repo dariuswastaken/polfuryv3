bae9791549578385cd23e6ead80a3569bd7e55e3
halva1337 <valentindarius1702@gmail.com>
2024-10-09 13:47:17 +0200

    changed test expectance

--- tests/main/botconfig.test.js
+++ tests/main/botconfig.test.js
@@ -2,7 +2,7 @@ const botconfig = require('../../src/botconfig/botconfig.js');
 
 describe('botconfig', () => {
   it('should export an object', () => {
-    expect(botconfig).toBe('object');
+    expect(botconfig).toBe(expect.any(Object));
     expect(botconfig).not.toBe(null);
   });
 });
