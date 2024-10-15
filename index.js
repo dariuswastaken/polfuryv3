f80810300ed0ad5023fcb769afa94c70cb7e8dd2
halva1337 <valentindarius1702@gmail.com>
2024-10-15 04:10:39 +0200

    testing

--- index.js
+++ index.js
@@ -3,4 +3,19 @@ import init from './src/core/init.js';
 const dotenv = require('dotenv');
 dotenv.config();
 
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { exportModules } from './src/core/baseExportFSModule.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const pulsarDiscordModules = exportModules(
+  path.join(__dirname, './pulsar/src/functions/discord')
+);
+
+console.log(pulsarDiscordModules);
+
+
 init();
--- pulsar/index.pulsar.js
+++ pulsar/index.pulsar.js
@@ -1,18 +1,5 @@
 import PulsarClient from './src/client.pulsar.js';
 
-import path from 'node:path';
-import { exportModules } from '../src/core/baseExportFSModule.js';
-import { fileURLToPath } from 'node:url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const pulsarDiscordModules = exportModules(
-  path.join(__dirname, '../src/functions/discord')
-);
-
 export const Pulsar = function () {
-  console.log(pulsarDiscordModules);
-  
   return new PulsarClient();
 };
