4e2b5e475564b5dc88fdd2b9f70789ce7f619c5b
halva1337 <valentindarius1702@gmail.com>
2024-10-15 03:47:29 +0200

    import fix

--- pulsar/src/client.pulsar.js
+++ pulsar/src/client.pulsar.js
@@ -1,8 +1,8 @@
-import { utilsManager } from '../managers/utilsManager.js';
-import { discordManager } from '../managers/discordManager.js';
-import { fileManager } from '../managers/fileManager.js';
-import { processManager } from '../managers/processManager.js';
-import { webManager } from '../managers/webManager.js';
+import utilsManager from '../managers/utilsManager.js';
+import discordManager from '../managers/discordManager.js';
+import fileManager from '../managers/fileManager.js';
+import processManager from '../managers/processManager.js';
+import webManager from '../managers/webManager.js';
 import * as clients from '../exports/clients.js';
 import config from '../.config/config.json' with { type: 'json' };
 
