3c742cbd0e80a00aa0c1dbdf8b27378a3c773ce3
halva1337 <valentindarius1702@gmail.com>
2024-10-15 03:53:11 +0200

    solved __dirname missing by defaukt

--- src/imports/dirs.js
+++ src/imports/dirs.js
@@ -1,4 +1,8 @@
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const handlerDir = path.join(__dirname, '../../discord/handlers');
 const eventDir = path.join(__dirname, '../../discord/events');
