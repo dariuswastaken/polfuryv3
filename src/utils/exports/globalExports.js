a12c0b6773f989b2c37efc9a3b3f8bfd7ff8d394
halva1337 <valentindarius1702@gmail.com>
2024-10-16 01:56:02 +0200

    path fix

--- src/utils/exports/globalExports.js
+++ src/utils/exports/globalExports.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const baseUtilModules = await exportModules(
-  path.join(__dirname, './base-util-exports')
+  path.join(__dirname, './core-util-exports')
 );
 
 /*const glbobalExports = {
