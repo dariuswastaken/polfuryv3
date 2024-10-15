0721d12a4884a8726476a92fcc017dfcd49d5a0a
halva1337 <valentindarius1702@gmail.com>
2024-10-15 21:07:57 +0200

    bug fixing and testing

--- index.js
+++ index.js
@@ -3,8 +3,4 @@ import init from './src/core/init.js';
 import dotenv from 'npm:dotenv';
 dotenv.config();
 
-import baseUtilModules from './src/utils/exports/globalExports.js';
-
-console.log(baseUtilModules);
-
 init.trigger();
