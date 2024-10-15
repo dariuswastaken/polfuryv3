9969369340c3a81d0f90b06a6bdca993b31dd7d0
halva1337 <valentindarius1702@gmail.com>
2024-10-15 21:37:39 +0200

    bug fixing and testing

--- src/core/baseExportFSModule.js
+++ src/core/baseExportFSModule.js
@@ -3,11 +3,14 @@ import path from 'node:path';
 
 export const exportModules = async (dir) => {
   const modules = {};
+  console.log(`[FILESYSTEM] Exporting modules from ${dir}`);
 
   const files = fs.readdirSync(dir);
+  console.log(`[FILESYSTEM] Found ${files.length} files in ${dir}`);
 
   await Promise.all(
     files.map(async (file) => {
+      console.log(`[FILESYSTEM] Processing file ${file}`);
       const filePath = path.join(dir, file);
       const stat = fs.statSync(filePath);
 
