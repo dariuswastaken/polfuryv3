62d1e40edc65feed3f946150c75b19f5caf80909
halva1337 <valentindarius1702@gmail.com>
2024-10-15 22:47:51 +0200

    testing & bug fixing

--- src/utils/exports/globalExports.js
+++ src/utils/exports/globalExports.js
@@ -1,4 +1,4 @@
-import { exportModules } from '../../core/baseExportFSModule.js';
+import { exportModules } from '../../fs/baseExportFSModule.js';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
