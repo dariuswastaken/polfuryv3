09451b3063531c80f1d08a5e7d5100e52823764b
halva1337 <valentindarius1702@gmail.com>
2024-10-15 20:43:18 +0200

    bug fixing and testing

--- src/utils/exports/globalExports.js
+++ src/utils/exports/globalExports.js
@@ -5,6 +5,30 @@ import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const baseUtilModules = path.join(__dirname, './base-utils-exports');
+const baseUtilModules = exportModules(
+  path.join(__dirname, './base-util-exports')
+);
 
-export default baseUtilModules;
+const globalExports = {
+  timeConversion: baseUtilModules.baseFuncExports.timeConversion,
+  dayConversion: baseUtilModules.baseFuncExports.dayConversion,
+  math: baseUtilModules.baseFuncExports.math,
+  checks: baseUtilModules.baseFuncExports.checks,
+  discord: {
+    buttonMenus: baseUtilModules.buttonMenuExports.buttonMenus,
+    embeds: baseUtilModules.embedExports.embeds,
+    errors: baseUtilModules.errorEmbedExports.errors,
+    validate: baseUtilModules.inputValidationExports.validate,
+    logging: baseUtilModules.loggingExports.logging,
+    modals: baseUtilModules.modalExports.modals,
+    quickFunctions: baseUtilModules.quickFuncExports.quickFunctions,
+    roles: baseUtilModules.roleExports.roles,
+    selectMenus: baseUtilModules.selectMenuExports.selectMenus
+  },
+  algorithms: baseUtilModules.algExports.algos,
+  activity: {
+    utils: baseUtilModules.activityUtilExports.utils
+  }
+};
+
+export default globalExports;
