856e037339eb7ed4da1d2038dfaf66ab9d1e6ac2
halva1337 <valentindarius1702@gmail.com>
2024-10-15 21:36:17 +0200

    bug fixing and testing

--- src/core/baseExportFSModule.js
+++ src/core/baseExportFSModule.js
@@ -6,17 +6,27 @@ export const exportModules = async (dir) => {
 
   const files = fs.readdirSync(dir);
 
-  await Promise.all(files.map(async (file) => {
-    const filePath = path.join(dir, file);
-    const stat = fs.statSync(filePath);
+  await Promise.all(
+    files.map(async (file) => {
+      const filePath = path.join(dir, file);
+      const stat = fs.statSync(filePath);
 
-    if (stat.isDirectory()) {
-      Object.assign(modules, await exportModules(filePath));
-    } else if (file.endsWith('.js')) {
-      const moduleName = path.basename(file, path.extname(file));
-      modules[moduleName] = await import(filePath);
-    }
-  }));
+      if (stat.isDirectory()) {
+        const subModules = await exportModules(filePath);
+        Object.assign(modules, subModules);
+      } else if (file.endsWith('.js')) {
+        const moduleName = path.basename(file, path.extname(file));
+        try {
+          modules[moduleName] = await import(filePath);
+        } catch (e) {
+          console.error(
+            `[FILESYSTEM] Error importing module ${moduleName} from ${filePath}`
+          );
+          console.error(e);
+        }
+      }
+    })
+  );
 
   return modules;
 };
