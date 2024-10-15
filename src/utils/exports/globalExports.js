112e5bcc1378cc26bc4c07295429e2e9d7faedfb
halva1337 <valentindarius1702@gmail.com>
2024-10-15 22:53:59 +0200

    testing & bug fixing

--- src/utils/exports/globalExports.js
+++ src/utils/exports/globalExports.js
@@ -1,10 +1,12 @@
-import { exportModules } from '../../fs/baseExportFSModule.js';
+import { exportAll } from './globalFSExport';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const baseUtilModules = {}
+const baseUtilModules = await exportAll(
+  path.join(__dirname, './base-util-exports')
+);
 
 export default baseUtilModules;
---
+++ src/utils/exports/globalFSExport.js
@@ -0,0 +1,34 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+// Temporary to avoid circular dependency
+
+export const exportAll = async (dir) => {
+  const modules = {};
+
+  const files = fs.readdirSync(dir);
+
+  await Promise.all(
+    files.map(async (file) => {
+      const filePath = path.join(dir, file);
+      const stat = fs.statSync(filePath);
+
+      if (stat.isDirectory()) {
+        const subModules = await exportModules(filePath);
+        Object.assign(modules, subModules);
+      } else if (file.endsWith('.js')) {
+        const moduleName = path.basename(file, path.extname(file));
+        try {
+          modules[moduleName] = await import(filePath);
+        } catch (e) {
+          console.error(
+            `[FILESYSTEM] Error importing module ${moduleName} from ${filePath}`
+          );
+          console.error(e);
+        }
+      }
+    })
+  );
+
+  return modules;
+};
