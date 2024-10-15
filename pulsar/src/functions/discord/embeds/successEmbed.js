37471846b2d28dea195b97fa39520baa7c99d4e5
halva1337 <valentindarius1702@gmail.com>
2024-10-15 03:59:24 +0200

    forgot a few module.exports

--- pulsar/src/functions/discord/embeds/successEmbed.js
+++ pulsar/src/functions/discord/embeds/successEmbed.js
@@ -1,53 +1,51 @@
 import Discord from 'npm:discord.js';
 
-module.exports = {
-  createSuccessEmbed: async (type, description, options) => {
-    const embed = new Discord.EmbedBuilder()
-      .setTitle(type)
-      .setDescription(description)
-      .setColor('#50C878');
+export const createSuccessEmbed = async (type, description, options) => {
+  const embed = new Discord.EmbedBuilder()
+    .setTitle(type)
+    .setDescription(description)
+    .setColor('#50C878');
 
-    const {
-      fields,
-      footer,
-      interaction,
-      message,
-      channel,
-      components,
-      edit,
-      ephemeral,
-      deferReply,
-      guild
-    } = options;
+  const {
+    fields,
+    footer,
+    interaction,
+    message,
+    channel,
+    components,
+    edit,
+    ephemeral,
+    deferReply,
+    guild
+  } = options;
 
-    if (fields) embed.addFields(fields);
-    if (footer) embed.setFooter(footer);
+  if (fields) embed.addFields(fields);
+  if (footer) embed.setFooter(footer);
 
-    const replyOptions = { embeds: [embed], ephemeral, components };
+  const replyOptions = { embeds: [embed], ephemeral, components };
 
-    if (interaction) {
-      if (deferReply) {
-        await interaction.editReply(replyOptions);
-      } else {
-        await interaction.reply(replyOptions);
-      }
-    } else if (message) {
-      if (edit) {
-        await message.edit(replyOptions);
-      } else {
-        await message.channel.send(replyOptions);
-      }
-    } else if (channel) {
-      if (!guild) {
-        return console.error(
-          new Error('[PULSAR] No guild specified for SUCCESS_EMBED')
-        );
-      }
-
-      const targetChannel = await guild.channels.cache.get(channel);
-      await targetChannel.send(replyOptions);
+  if (interaction) {
+    if (deferReply) {
+      await interaction.editReply(replyOptions);
     } else {
-      return console.error(new Error('[PULSAR] Invalid event type'));
+      await interaction.reply(replyOptions);
     }
+  } else if (message) {
+    if (edit) {
+      await message.edit(replyOptions);
+    } else {
+      await message.channel.send(replyOptions);
+    }
+  } else if (channel) {
+    if (!guild) {
+      return console.error(
+        new Error('[PULSAR] No guild specified for SUCCESS_EMBED')
+      );
+    }
+
+    const targetChannel = await guild.channels.cache.get(channel);
+    await targetChannel.send(replyOptions);
+  } else {
+    return console.error(new Error('[PULSAR] Invalid event type'));
   }
 };
