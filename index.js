287d0697e4f9db89b7e1380a3970c659d5bad824
halva1337 <valentindarius1702@gmail.com>
2024-10-15 20:21:07 +0200

    bug fixing and testing

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import dotenv from 'npm:dotenv';
 dotenv.config();
 
 import { exportModules } from './src/core/baseExportFSModule.js';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
