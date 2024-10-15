9d5612855aeef1ac48154fd8c3a7ec7e31cf118b
halva1337 <valentindarius1702@gmail.com>
2024-10-15 14:23:55 +0200

    bug fixing

--- src/mongo/functions/activityQueries/baseActivityQueries.js
+++ src/mongo/functions/activityQueries/baseActivityQueries.js
@@ -1,5 +1,5 @@
 import { db } from '../../../handlers/mongoConnectionHandler.js';
-import { dayConversion } from '../../../utils/exports/globalExports.js';
+import { dayConversion } from '../../../utils/functions/base/dayConversion.js';
 
 export const updateActivity = async (discordID, serverID, { pontaj, rapoarte, amenzi, apeluri, lastLogin }) => {
   const currentWeek = await dayConversion.getCurrentWeek();
