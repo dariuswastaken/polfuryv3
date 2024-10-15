91f5b1a8249f8f8a46c3a984c1c1e03f1a4cb783
halva1337 <valentindarius1702@gmail.com>
2024-10-15 20:20:30 +0200

    bug fixing and testing

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ import init from './src/core/init.js';
 import dotenv from 'npm:dotenv';
 dotenv.config();
 
-import utils from './src/utils/exports/globalExports.js';
-console.log(utils);
+import { exportModules } from './src/core/baseExportFSModule.js';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const baseUtilModules = await exportModules(
+  path.join(__dirname, './src/utils/exports/base-util-exports')
+);
+
+console.log(baseUtilModules);
+
 
 init.trigger();
