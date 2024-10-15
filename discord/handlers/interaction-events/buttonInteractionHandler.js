68446b9c030307c1ee5b3f226b77dfc425cdeaea
halva1337 <valentindarius1702@gmail.com>
2024-10-15 23:07:24 +0200

    testing & bug fixing

--- discord/handlers/interaction-events/buttonInteractionHandler.js
+++ discord/handlers/interaction-events/buttonInteractionHandler.js
@@ -9,7 +9,7 @@ export default {
       if (interaction.user.bot) return;
       if (!interaction.isButton()) return;
       
-      console.log('PAssed')
+      console.log(utils)
       
       return;
       
