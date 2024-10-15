6913b659dbb988e96af9badb1143b8d203265caf
halva1337 <valentindarius1702@gmail.com>
2024-10-15 20:27:22 +0200

    bug fixing and testing

--- index.js
+++ index.js
@@ -3,17 +3,8 @@ import init from './src/core/init.js';
 import dotenv from 'npm:dotenv';
 dotenv.config();
 
-import { exportModules } from './src/core/baseExportFSModule.js';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const baseUtilModules = await exportModules(
-  path.join(__dirname, './pulsar/exports')
-);
+import baseUtilModules from './src/utils/exports/globalExports.js';
 
 console.log(baseUtilModules);
-
 
 init.trigger();
--- src/utils/exports/globalExports.js
+++ src/utils/exports/globalExports.js
@@ -9,26 +9,4 @@ const baseUtilModules = await exportModules(
   path.join(__dirname, './base-util-exports')
 );
 
-const globalExports = {
-  timeConversion: baseUtilModules.baseFuncExports.timeConversion,
-  dayConversion: baseUtilModules.baseFuncExports.dayConversion,
-  math: baseUtilModules.baseFuncExports.math,
-  checks: baseUtilModules.baseFuncExports.checks,
-  discord: {
-    buttonMenus: baseUtilModules.buttonMenuExports.buttonMenus,
-    embeds: baseUtilModules.embedExports.embeds,
-    errors: baseUtilModules.errorEmbedExports.errors,
-    validate: baseUtilModules.inputValidationExports.validate,
-    logging: baseUtilModules.loggingExports.logging,
-    modals: baseUtilModules.modalExports.modals,
-    quickFunctions: baseUtilModules.quickFuncExports.quickFunctions,
-    roles: baseUtilModules.roleExports.roles,
-    selectMenus: baseUtilModules.selectMenuExports.selectMenus
-  },
-  algorithms: baseUtilModules.algExports.algos,
-  activity: {
-    utils: baseUtilModules.activityUtilExports.utils
-  }
-};
-
-export default globalExports;
+export default baseUtilModules;
