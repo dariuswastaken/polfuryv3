608c59355db185c46ce4c3ce66e0c10bf664e387
halva1337 <valentindarius1702@gmail.com>
2024-10-15 03:51:24 +0200

    import fix

--- src/core/init.js
+++ src/core/init.js
@@ -1,4 +1,4 @@
-import fileHandler from '../handlers/fileHandler.js';
+import * as fileHandler from '../handlers/fileHandler.js';
 import { Pulsar } from '../../pulsar/index.pulsar.js';
 
 const client = Pulsar().client;
