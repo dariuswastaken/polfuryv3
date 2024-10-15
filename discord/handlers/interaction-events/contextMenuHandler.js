476b22ddbb341d4445dcc27a5248cde7f4b637ce
halva1337 <valentindarius1702@gmail.com>
2024-10-15 14:49:18 +0200

    bug fixing

--- discord/handlers/interaction-events/contextMenuHandler.js
+++ discord/handlers/interaction-events/contextMenuHandler.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const resolvedPath = path.resolve(__dirname, '../../src/utils/exports/globalExports.js');
+const resolvedPath = path.resolve(__dirname, '../../../src/utils/exports/globalExports.js');
 const modulePath = `file://${resolvedPath}`;
 
 const utils = await import(modulePath);
