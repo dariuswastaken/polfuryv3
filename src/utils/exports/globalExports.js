fd0353fcc868d5e93e6e70c4aab8a4a67f19c3da
halva1337 <valentindarius1702@gmail.com>
2024-10-15 21:20:27 +0200

    bug fixing and testing

--- src/utils/exports/globalExports.js
+++ src/utils/exports/globalExports.js
@@ -5,8 +5,14 @@ import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const baseUtilModules = await exportModules(
-  path.join(__dirname, './base-util-exports')
-);
+try {
+
+  const baseUtilModules = await exportModules(
+    path.join(__dirname, './base-util-exports')
+  );
+
+} catch (error) {
+  console.error(error);
+}
 
 export default baseUtilModules;
