99e3b6de87e0cf9102ab4a9ff77d6178de7b9de7
halva1337 <valentindarius1702@gmail.com>
2024-10-15 21:52:19 +0200

    Trying to fix a circular dependency

--- src/utils/exports/loadBaseUtilModules.js
+++ src/utils/exports/loadBaseUtilModules.js
@@ -1,5 +1,30 @@
-import { exportModules } from '../../core/baseExportFSModule.js';
-
 export const loadBaseUtilModules = async (dir) => {
-  return await exportModules(dir);
+  const modules = {};
+  console.log(`[FILESYSTEM] Exporting modules from ${dir}`);
+
+  const files = fs.readdirSync(dir);
+
+  await Promise.all(
+    files.map(async (file) => {
+      const filePath = path.join(dir, file);
+      const stat = fs.statSync(filePath);
+
+      if (stat.isDirectory()) {
+        const subModules = await loadBaseUtilModules(filePath);
+        Object.assign(modules, subModules);
+      } else if (file.endsWith('.js')) {
+        const moduleName = path.basename(file, path.extname(file));
+        try {
+          modules[moduleName] = await import(filePath);
+        } catch (e) {
+          console.error(
+            `[FILESYSTEM] Error importing module ${moduleName} from ${filePath}`
+          );
+          console.error(e);
+        }
+      }
+    })
+  );
+
+  return modules;
 };
