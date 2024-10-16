77da4c47fa0b53c65fecdf7d92b66e77e501d7d7
halva1337 <valentindarius1702@gmail.com>
2024-10-16 02:40:43 +0200

    fixing bugs

--- src/utils/exports/globalExports.js
+++ src/utils/exports/globalExports.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const baseUtilModules = await exportModules(
-  path.join(__dirname, '../../functions')
+  path.join(__dirname, '../functions')
 );
 
 for (const file of Object.keys(exportFiles)) {
