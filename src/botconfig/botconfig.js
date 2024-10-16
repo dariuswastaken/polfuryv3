268e6847610c684fc04fa096df647e48f13a40a2
halva1337 <valentindarius1702@gmail.com>
2024-10-16 02:30:42 +0200

    fixing bugs

--- src/botconfig/botconfig.js
+++ src/botconfig/botconfig.js
@@ -1,10 +1,10 @@
-import { exportModules } from '../../src/fs/baseExportFSModule.js';
+import { exportModules } from '../fs/baseExportFSModule.js';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const botconfig = {}
+const botconfig = await exportModules(__dirname);
 
 export default botconfig;
