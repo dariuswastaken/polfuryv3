e3fe265b920dc50d8790eb1c127c96d28b7f4f44
halva1337 <valentindarius1702@gmail.com>
2024-10-16 02:41:16 +0200

    fixing bugs

--- src/handlers/fileHandler.js
+++ src/handlers/fileHandler.js
@@ -2,8 +2,9 @@ import * as dirs from '../imports/dirs.js';
 import { Pulsar } from '../../pulsar/index.pulsar.js';
 import { printTable } from 'npm:console-table-printer';
 
-import { botconfig, utils } from '../utils/dependencies.js';
+import { utils } from '../utils/dependencies.js';
 
+const botconfig = {}
 const fileSystem = Pulsar().fileManager.createInstance();
 const client = Pulsar().client;
 let events = [];
--- src/utils/dependencies.js
+++ src/utils/dependencies.js
@@ -1,4 +1,3 @@
 import utils from './exports/globalExports.js';
-import botconfig from '../botconfig/botconfig.js';
 
-export { utils, botconfig };
+export { utils };
