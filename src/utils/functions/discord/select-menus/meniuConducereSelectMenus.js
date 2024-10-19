3e4033b241e315447a18546226183483b985b956
halva1337 <valentindarius1702@gmail.com>
2024-10-19 23:58:38 +0200

    added new week select function

--- src/utils/functions/discord/select-menus/meniuConducereSelectMenus.js
+++ src/utils/functions/discord/select-menus/meniuConducereSelectMenus.js
@@ -34,3 +34,31 @@ export const sendRankChoiceMenu = async ({ pulsar, interaction, targetID }) => {
     }
   );
 };
+
+export const sendWeekChoiceMenu = async ({ pulsar, interaction, mongo, title }) => {
+  const activityWeeks = await mongo.getAllActivityWeeks();
+  let options = [];
+  for (let week of activityWeeks) {
+    options.push({
+      label: `${week}`,
+      value: `${week}`
+    });
+  }
+  const menu = await pulsar.discordManager.menus.createSelectMenu({
+    type: 'string',
+    options: options,
+    id: `top-activity-week-select/${interaction.user.id}`,
+    placeholder: 'Alege o saptamana'
+  });
+
+  await pulsar.discordManager.embeds.createDefaultEmbed(
+    'Alege o saptamana din meniul de mai jos.',
+    {
+      title: `${title}`,
+      interaction: interaction,
+      components: [menu],
+      ephemeral: true,
+      deferReply: true
+    }
+  );
+};
