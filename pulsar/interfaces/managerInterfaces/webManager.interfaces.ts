ff77b69e8ce67b9933fcb7907b4786f67c652cf3
halva1337 <valentindarius1702@gmail.com>
2024-11-03 15:23:19 +0100

    removed resetMDT function

--- pulsar/interfaces/managerInterfaces/webManager.interfaces.ts
+++ pulsar/interfaces/managerInterfaces/webManager.interfaces.ts
@@ -2,5 +2,4 @@ export interface WebInstance {
     getUserMdtData: (userId: string) => Promise<any>;
     isOnDuty: (userId: string) => Promise<boolean>;
     getUserServerProfile: (userId: string) => Promise<any>;
-    resetMDT: () => Promise<void>;
 }
