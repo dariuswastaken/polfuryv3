b421cf826b42ad785f1f13a6fbb5bda8d7c0b6bc
halva1337 <valentindarius1702@gmail.com>
2024-11-03 15:11:46 +0100

    checking for undefined modules

--- pulsar/exports/discord.ts
+++ pulsar/exports/discord.ts
@@ -8,22 +8,22 @@ const __dirname = path.dirname(__filename);
 const pulsarDiscordModules = await exportModules(path.join(__dirname, '../src/functions/discord'));
 
 const embeds = {
-    createDefaultEmbed: pulsarDiscordModules.defaultEmbed.createDefaultEmbed,
-    createErrorEmbed: pulsarDiscordModules.errorEmbed.createErrorEmbed,
-    createSuccessEmbed: pulsarDiscordModules.successEmbed.createSuccessEmbed,
-    createWarningEmbed: pulsarDiscordModules.warningEmbed.createWarningEmbed,
-    createLogEmbed: pulsarDiscordModules.logEmbed.createLogEmbed,
-    createForumThreadEmbed: pulsarDiscordModules.forumThreadEmbed.createForumThreadEmbed,
-    createEmbed: pulsarDiscordModules.baseCustomizableEmbed.createEmbed
+    createDefaultEmbed: pulsarDiscordModules.defaultEmbed?.createDefaultEmbed,
+    createErrorEmbed: pulsarDiscordModules.errorEmbed?.createErrorEmbed,
+    createSuccessEmbed: pulsarDiscordModules.successEmbed?.createSuccessEmbed,
+    createWarningEmbed: pulsarDiscordModules.warningEmbed?.createWarningEmbed,
+    createLogEmbed: pulsarDiscordModules.logEmbed?.createLogEmbed,
+    createForumThreadEmbed: pulsarDiscordModules.forumThreadEmbed?.createForumThreadEmbed,
+    createEmbed: pulsarDiscordModules.baseCustomizableEmbed?.createEmbed
 };
 
 const menus = {
-    createSelectMenu: pulsarDiscordModules.selectMenuBuilder.createSelectMenu,
-    createButtonMenu: pulsarDiscordModules.buttonMenuBuilder.createButtonMenu
+    createSelectMenu: pulsarDiscordModules.selectMenuBuilder?.createSelectMenu,
+    createButtonMenu: pulsarDiscordModules.buttonMenuBuilder?.createButtonMenu
 };
 
 const modals = {
-    createModal: pulsarDiscordModules.modalBuilder.createModal
+    createModal: pulsarDiscordModules.modalBuilder?.createModal
 };
 
 export { embeds, menus, modals };
