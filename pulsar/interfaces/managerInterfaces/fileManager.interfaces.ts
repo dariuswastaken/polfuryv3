016b7d968cdedabc40fbc3742dfc754c812040a5
halva1337 <valentindarius1702@gmail.com>
2024-11-05 02:50:22 +0100

    loadFilesFromDir function type fix

--- pulsar/interfaces/managerInterfaces/fileManager.interfaces.ts
+++ pulsar/interfaces/managerInterfaces/fileManager.interfaces.ts
@@ -1,4 +1,4 @@
 export interface FileInstance {
     createLogsManager: () => void;
-    loadFilesFromDir: () => void;
+    loadFilesFromDir: (dir: string, callback: (filePath: string) => void) => void;
 }
