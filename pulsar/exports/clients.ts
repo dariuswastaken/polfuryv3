e2f9361dbe3617cf559128c009978ab7a06e19ec
halva1337 <valentindarius1702@gmail.com>
2024-11-05 02:35:27 +0100

    mongoClient import & export update

--- pulsar/exports/clients.ts
+++ pulsar/exports/clients.ts
@@ -1,4 +1,4 @@
 import client from '../src/core/discord/client.ts';
-import MongoClient from '../clients/mongoClient.ts';
+import { mongoClient } from '../clients/mongoClient.ts';
 
-export { client, MongoClient };
+export { client, mongoClient };
