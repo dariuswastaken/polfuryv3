01a2570c1931117f678778fb71971ed0a6f0e669
halva1337 <valentindarius1702@gmail.com>
2024-11-11 02:05:14 +0100

    added logging directly to discord

---
+++ discord/events/startup/discordLogging.js
@@ -0,0 +1,48 @@
+export default {
+    name: 'discordLogging',
+    async execute(Pulsar) {
+        Pulsar.client.on('ready', async () => {
+            const logsChannel = Pulsar.client.channels.cache.get('1305336008774127729');
+            const errorChannel = Pulsar.client.channels.cache.get('1305336008774127729');
+
+            const formattedTimeAndDate = time
+                .toLocaleString('de-DE', {
+                    timeZone: 'Europe/Berlin'
+                })
+                .replace(', ', ' ');
+
+            logsChannel.send(`[INFO] Restart at ${formattedTimeAndDate}`);
+            errorChannel.send(`[INFO] Restart at ${formattedTimeAndDate}`);
+
+            console.log = function (msg) {
+                const time = new Date();
+                const formattedTimeAndDate = time
+                    .toLocaleString('de-DE', {
+                        timeZone: 'Europe/Berlin'
+                    })
+                    .replace(', ', ' ');
+
+                if (typeof msg === 'object') {
+                    msg = util.inspect(msg);
+                }
+
+                logsChannel.send(`[${formattedTimeAndDate}] ${msg}`);
+            };
+
+            console.error = function (msg) {
+                const time = new Date();
+                const formattedTimeAndDate = time
+                    .toLocaleString('de-DE', {
+                        timeZone: 'Europe/Berlin'
+                    })
+                    .replace(', ', ' ');
+
+                if (typeof msg === 'object') {
+                    msg = util.inspect(msg);
+                }
+
+                errorChannel.send(`[${formattedTimeAndDate}] ${msg}`);
+            };
+        });
+    }
+};
